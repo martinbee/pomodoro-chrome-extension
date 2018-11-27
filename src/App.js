428ffe96a968f99f56a44cfd487efe28d3bb07fb
Martin <martbee88@gmail.com>
2018-11-26 20:53:45 -0600

    feat: begin work on code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+function convertMinutesToMilliseconds(minutes) {
+  return minutes * 60 * 1000;
+}
+
 class App extends Component {
+  state = {
+    isActive: true,
+    activeDuration: 25,
+    restDuration: 5,
+  }
+
+  startTimer = () => {
+    const {
+      isActive,
+      activeDuration,
+      restDuration,
+    } = this.state;
+
+    const message = isActive ?
+      'Take a break!' :
+      'Back to work!';
+    const duration = isActive ? activeDuration : restDuration;
+
+    this.timer = setTimeout(() => {
+      this.setState({ isActive: !isActive });
+      alert(message);
+      this.startTimer();
+    }, 5000);
+  }
+
+  stopTimer = () => {
+    clearTimeout(this.timer);
+    this.setState({ isActive: true });
+  }
+
+  renderButton() {
+    console.log(this);
+    if (this.state.isActive) {
+      return <button onClick={this.startTimer}>Start</button>;
+    }
+
+    return <button onClick={this.stopTimer}>Stop</button>;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.renderButton()}
         </header>
       </div>
     );
