5c13bf71be7bacf6fa88f5cd7de5e2efe2b5b688
Martin Bee <martinb@nobrainer2.com>
2018-11-28 11:56:06 -0600

    feat: get alerts working; logic is broken right now

--- public/background.js
+++ public/background.js
@@ -1,7 +1,34 @@
 /* global chrome */
 const state = {
   isTimerRunning: false,
+  isWorkTimer: null,
 };
+
+const workDuration = 20;
+const breakDuration = 5;
+
+const timerName = 'pomodoro-timer';
+
+const toggleTimer = () => {
+  if (state.isTimerRunning) {
+    chrome.alarms.clear(timerName);
+    state.isWorkTimer = null;
+  } else {
+    chrome.alarms.create(timerName, { delayInMinutes: 1 });
+    state.isWorkTimer = true;
+  }
+  
+  state.isTimerRunning = !state.isTimerRunning;
+};
+
+chrome.alarms.onAlarm.addListener(({ name }) => {
+  if (name === timerName) {
+    const message = state.isWorkTimer ? 'Take a break!' : 'Get back to work slacker!';
+    
+    alert(message);
+    chrome.alarms.create(timerName, { delayInMinutes: 1 });
+  }
+});
 
 chrome.runtime.onConnect.addListener((port) => {
   console.assert(port.name === 'pomodoro-timer');
@@ -16,7 +43,8 @@ chrome.runtime.onConnect.addListener((port) => {
         break;
         
       case 'toggleTimer':
-        state.isTimerRunning = !state.isTimerRunning; 
+        toggleTimer();
+        
         port.postMessage({
           type: 'toggleTimer',
           payload: state,
